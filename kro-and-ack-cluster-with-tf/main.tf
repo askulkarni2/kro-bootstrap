f7be12a74ef20d392e3ea01a8ce654372f051df7
Apoorva Kulkarni <kuapoorv@amazon.com>
2025-01-22 21:31:37 -0800

    use auto-mode

--- kro-and-ack-cluster-with-tf/main.tf
+++ kro-and-ack-cluster-with-tf/main.tf
@@ -65,29 +65,126 @@ locals {
 
   tags = {
     Blueprint  = local.name
-    GithubRepo = "github.com/awslabs/crossplane-on-eks"
+    GithubRepo = "terraform-aws-eks-blueprints"
+    GithubOrg  = "aws-ia"
   }
 }
 
-#---------------------------------------------------------------
-# EBS CSI Driver Role
-#---------------------------------------------------------------
+#########################################
+# EKS
+#########################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.33"
 
-module "ebs_csi_driver_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.30"
+  cluster_name                   = local.name
+  cluster_version                = local.cluster_version
+  cluster_endpoint_public_access = true
 
-  role_name = "${local.name}-ebs-csi-driver"
+  # Give the Terraform identity admin access to the cluster
+  # which will allow resources to be deployed into the cluster
+  enable_cluster_creator_admin_permissions = true
 
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   tags = local.tags
 }
 
+resource "kubectl_manifest" "configmap" {
+  yaml_body = templatefile("${path.module}/configmap.yaml", {
+    awsAccountID = data.aws_caller_identity.current.account_id
+    eksOIDC      = module.eks.oidc_provider
+    vpcID        = module.vpc.vpc_id
+    subnetIDs    = join(",", module.vpc.private_subnets)
+    clusterName  = module.eks.cluster_name
+    region       = local.region
+  })
+
+  depends_on = [module.eks]
+}
+
+################################################################################
+# ACK
+################################################################################
+module "eks_ack_addons" {
+  source = "aws-ia/eks-ack-addons/aws"
+
+  # Cluster Info
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  # ECR Credentials
+  ecrpublic_username = data.aws_ecrpublic_authorization_token.token.user_name
+  ecrpublic_token    = data.aws_ecrpublic_authorization_token.token.password
+
+  # Controllers to enable
+  enable_iam = true
+  iam = {
+    chart_version = "1.3.13"
+  }
+
+  enable_eks = true
+  eks = {
+    chart_version = "1.5.3"
+  }
+  
+  enable_s3  = true
+  s3 = {
+    chart_version = "1.0.20"
+  }
+  
+  tags = local.tags
+}
+
+#########################################
+# KRO
+#########################################
+module "kro" {
+  source  = "aws-ia/eks-blueprints-addon/aws"
+  version = "1.1.1"
+
+  name             = "kro"
+  description      = "A Helm chart to deploy kro"
+  namespace        = "kro"
+  create_namespace = true
+  chart            = "kro"
+  chart_version    = "0.1.0"
+  repository       = "oci://public.ecr.aws/kro"
+}
+
+#########################################
+# VPC
+#########################################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  manage_default_vpc = true
+
+  name = local.vpc_name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = local.tags
+}
--- kro-and-ack-cluster-with-tf/versions.tf
+++ kro-and-ack-cluster-with-tf/versions.tf
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 terraform {
   required_version = ">= 1.3"
 
